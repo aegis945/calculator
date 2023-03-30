8eb8c9f916efc491c6d295f79391ac6b6f395160
aegis945 <artiom.lekincev@gmail.com>
2023-03-30 16:57:27 +0300

    display the input numbers and AC btn functionality

--- scripts.js
+++ scripts.js
@@ -6,20 +6,23 @@ const allClearButton = document.querySelector("[data-all-clear]");
 const deleteButton = document.querySelector("[data-delete]");
 const equalsButton = document.querySelector("[data-equals]");
 
+
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
-
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
     })
 })
 
 operationButtons.forEach(button => {
     button.addEventListener("click", () => {
-
+        console.log(button.innerText);
     })
 })
 
 allClearButton.addEventListener("click", () => {
-
+    calculator.allClear();
+    calculator.updateDisplay();
 })
 
 deleteButton.addEventListener("click", () => {
@@ -27,5 +30,33 @@ deleteButton.addEventListener("click", () => {
 })
 
 equalsButton.addEventListener("click", () => {
-
+   
 })
+
+class Calculator {
+    constructor(previousOperandText, currentOperandText) {
+        this.previousOperandText = previousOperandText;
+        this.currentOperandText = currentOperandText;
+        this.allClear();
+    }
+
+    appendNumber(number) {
+        if(this.currentOperand === 0){
+            this.currentOperand = number.toString();
+        } else {
+            this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+}
+
+    updateDisplay() {
+        this.currentOperandText.innerText = this.currentOperand;
+        this.previousOperandText.innerText = this.previousOperand;
+    }
+
+    allClear(){
+        this.currentOperand = 0;
+        this.previousOperand = "";
+    }
+}
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
