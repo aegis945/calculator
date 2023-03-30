a90f148f16b77ef87e7d13934ba92a9d03344ad5
aegis945 <artiom.lekincev@gmail.com>
2023-03-30 14:37:43 +0300

    preliminary selected buttons and event listeners, added hover effect

--- scripts.js
+++ scripts.js
@@ -1,1 +1,31 @@
-/* this is just a test */
+const previousOperandText = document.querySelector("[data-previous-operand]");
+const currentOperandText = document.querySelector("[data-current-operand]");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const deleteButton = document.querySelector("[data-delete]");
+const equalsButton = document.querySelector("[data-equals]");
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+    })
+})
+
+allClearButton.addEventListener("click", () => {
+
+})
+
+deleteButton.addEventListener("click", () => {
+
+})
+
+equalsButton.addEventListener("click", () => {
+
+})
